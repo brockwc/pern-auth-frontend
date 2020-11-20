e512e1798010bc4e07c05bbb4a92b44f667f8ff3
laurennguyen395 <laurennguyen395@gmail.com>
2020-11-20 14:26:01 -0800

    associated tables correctl to display user info on profile page

--- src/models/userBenefit.js
+++ src/models/userBenefit.js
@@ -1,19 +1,17 @@
 const REACT_APP_API_URL = "http://localhost:4000/api/v1"
+const userId = localStorage.getItem('id')
 
 class UserBenefitModel {
 
     static all = () => {
         // Calling the index method in the API controller
-        return fetch(`${REACT_APP_API_URL}/profile`).then(res => res.json())
+        return fetch(`${REACT_APP_API_URL}/profile/${userId}`).then(res => res.json())
     }
 
     static show = (userBenefitId) => {
         return fetch(`${REACT_APP_API_URL}/profile/${userBenefitId}`).then(res => res.json())
     }
 }
-
-
-console.log('helllloooo')
 
 
 export default UserBenefitModel;
