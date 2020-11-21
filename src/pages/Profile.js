de336f68a1d139b02ef86620794164a555b1ae62
laurennguyen395 <laurennguyen395@gmail.com>
2020-11-20 16:18:09 -0800

    displaying user name and users saved benefits on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,27 +4,30 @@ import UserBenefitModel from "../models/userBenefit";
 
 const Profile = (props) => {
   const [userBenefits, setUserBenefits] = useState([])
+  const [userInfo, setUserInfo] = useState(null)
 
   const fetchUserBenData = () => {
     UserBenefitModel.all().then(data => {
-        console.log(data.userBenefits)
-        setUserBenefits(data.userBenefits)
+      { console.log(data.userBenefits[0].benefits) }
+      { console.log(data.userBenefits[0].firstName) }
+      setUserBenefits(data.userBenefits[0].benefits)
+      setUserInfo(data.userBenefits[0].firstName)
     })
-}
+  }
 
-useEffect(() => {
-  fetchUserBenData()
-}, [])
+  useEffect(() => {
+    fetchUserBenData()
+  }, [])
 
 
   return (
     <div>
-      <h1>Profile of user with ID { props.currentUser }</h1>
+      <h1>Hello {userInfo}</h1>
       {userBenefits.map((userBenefit, index) => {
         return <p>{userBenefit.name}</p>
       })}
     </div>
-    
+
   )
 }
 
