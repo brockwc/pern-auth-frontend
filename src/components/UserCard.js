497be6d6a54b0ce7469a6c1a07bf4773e240c517
laurennguyen395 <laurennguyen395@gmail.com>
2020-11-28 13:29:21 -0800

    got user info properly

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,15 +1,84 @@
-import React from 'react';
-
-const UserCard = (props) => {
-
-    // console.log(props)
+import React, { useEffect, useState } from 'react';
 
 
-return(
-    <>
-    <h1>user info and update info</h1>
-    </>
-)
+const UserCard = ({user}) => {
+    const [newFirstName, setNewFirstName] = useState('');
+    const [newLastName, setNewLastName] = useState('');
+    const [newEmail, setNewEmail] = useState('');
+
+    useEffect(() => {
+        if(user){
+            setNewFirstName(user.firstName);
+            setNewLastName(user.lastName);
+            setNewEmail(user.email);
+        }
+    }, [user])
+
+    console.log(user)
+
+    const handleNewFirstName = e => {
+        setNewFirstName(e.target.value)
+    }
+    const handleNewLastName = e => {
+        setNewLastName(e.target.value)
+    }
+    const handleNewEmail = e => {
+        setNewEmail(e.target.value)
+    }
+    const handleNewSubmit = e => {
+        e.preventDefault()
+        // if (password === confirmPassword) {
+        //     UserModel.create({ firstName, lastName, email, password })
+        //         .then(data => {
+        //             console.log('Successful register', data)
+        //             // redirect to /login
+        //             props.history.push('/profile')
+        //         })
+        // }
+    }
+    return (
+        <div className="RegDiv">
+            <h2>Update User Info</h2>
+            <form onSubmit={handleNewSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">First Name:</label>
+                    <input
+                        onChange={handleNewFirstName}
+                        value={newFirstName}
+                        type="text"
+                        id="name"
+                        name="name"
+                        //placeholder
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Last Name:</label>
+                    <input
+                        onChange={handleNewLastName}
+                        value={newLastName}
+                        type="text"
+                        id="name"
+                        name="name"
+                        //placeholder
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Email:</label>
+                    <input
+                        onChange={handleNewEmail}
+                        value={newEmail}
+                        type="email"
+                        id="email"
+                        name="email"
+                        //placeholder
+                        required
+                    />
+                </div>
+                <button type="submit">Update</button>
+            </form>
+        </div>
+    )
 }
-
 export default UserCard;
